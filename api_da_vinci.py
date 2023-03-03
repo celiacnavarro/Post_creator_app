d57b99e79c55c18bddbff0a846571b6406d2e554
celiacnavarro <celiacnavarro@gmail.com>
2023-03-03 19:44:16 +0100

    db

--- api_da_vinci.py
+++ api_da_vinci.py
@@ -15,6 +15,25 @@ openai.api_key = os.getenv("ACCESS_KEY")
 
 app = Flask(__name__)
 
+#Credenciales
+username = "admin"
+password = "proyecto"
+host = "database-1.cnvu29xw2umg.us-east-2.rds.amazonaws.com" 
+port = 3306
+#Conexión DB
+db = pymysql.connect(host = host,
+                     user = username,
+                     password = password,
+                     cursorclass = pymysql.cursors.DictCursor
+)
+cursor = db.cursor()
+# Seleccionar DB
+
+cursor.connection.commit()
+use_db = ''' USE prompt_database'''
+cursor.execute(use_db)
+
+
 # Definir la ruta de la página principal
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -33,12 +52,12 @@ def index():
 
         # Guardar pregunta y respuesta en la base de datos
         time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        cursor = mysql.connection.cursor()
         cursor.execute('''INSERT INTO prompt (time, question, answer)
                       VALUES (%s, %s, %s)''',
                    (time, user_input, output))
-        mysql.connection.commit()
-
+        db.connection.commit()
+        db.close()
+        
         # Renderizar la plantilla del formulario con la respuesta de GPT-3
         return render_template('index.html', output=output)
     else:
