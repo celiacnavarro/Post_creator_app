2bff8175a39c05c734d888b464f2800a2872e4c3
celiacnavarro <celiacnavarro@gmail.com>
2023-03-03 19:35:45 +0100

    connection db

--- api_da_vinci.py
+++ api_da_vinci.py
@@ -2,13 +2,16 @@ import os
 import openai
 from flask import Flask, request, jsonify, render_template
 from dotenv import load_dotenv
+import datetime 
+from flask_mysqldb import MySQL
+import pymysql
+
+
+os.chdir(os.path.dirname(__file__))
 
 # Traernos la API_KEY
 load_dotenv()
-
-ACCESS_KEY = os.getenv("ACCESS_KEY")
-
-openai.api_key = ACCESS_KEY
+openai.api_key = os.getenv("ACCESS_KEY")
 
 app = Flask(__name__)
 
@@ -27,7 +30,15 @@ def index():
             temperature=0.8,
         )
         output = response.choices[0].text
-        
+
+        # Guardar pregunta y respuesta en la base de datos
+        time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cursor = mysql.connection.cursor()
+        cursor.execute('''INSERT INTO prompt (time, question, answer)
+                      VALUES (%s, %s, %s)''',
+                   (time, user_input, output))
+        mysql.connection.commit()
+
         # Renderizar la plantilla del formulario con la respuesta de GPT-3
         return render_template('index.html', output=output)
     else:
